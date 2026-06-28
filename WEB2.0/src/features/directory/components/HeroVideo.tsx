88d001a92097aa11633b22d81027404c32c9d4e8
UN CAPO <you@example.com>
2026-06-28 14:01:02 -0600

    feature/arreglo de estructura2.0

--- WEB2.0/src/features/directory/components/HeroVideo.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import HeroSearch from '@/src/features/directory/components/HeroSearch';
-
-export default function HeroVideo() {
-  return <HeroSearch videoSrc="/videos/doctor_writting.mp4" />;
-}
